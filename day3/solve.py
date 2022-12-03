adaaec4796de9d9c3880c99e74b8fcafc02d67c2
forden <forden@pop-os.localdomain>
2022-12-04 00:41:20 +0300

    day 3 part 2 completed

--- day3/solve.py
+++ day3/solve.py
@@ -5,11 +5,12 @@ with open('input.txt') as f:
 lines = [i.strip() for i in lines]
 
 
+def get_item_priority(item: str):
+    return string.ascii_letters.index(item) + 1
+
+
 def part_1():
     priorities_score = 0
-
-    def get_item_priority(item: str):
-        return string.ascii_letters.index(item) + 1
 
     for rucksack in lines:
         first_part, second_part = rucksack[:len(rucksack) // 2], rucksack[len(rucksack) // 2:]
@@ -20,4 +21,17 @@ def part_1():
     return priorities_score
 
 
+def part_2():
+    priorities_score = 0
+
+    for first_rucksack_id in range(0, len(lines), 3):
+        rucksacks_group = lines[first_rucksack_id:first_rucksack_id + 3]
+        intersection = set(rucksacks_group[0]).intersection(*rucksacks_group[1:])
+        for i in intersection:
+            priorities_score += get_item_priority(i)
+
+    return priorities_score
+
+
 print(part_1())
+print(part_2())
