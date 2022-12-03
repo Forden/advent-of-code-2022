32588a61ddecf4b4f8a4dc885b1e23ce0baafc9d
forden <forden@pop-os.localdomain>
2022-12-03 23:50:03 +0300

    day 1 part 1 completed

---
+++ day1/solve.py
@@ -0,0 +1,12 @@
+with open('input.txt') as f:
+    lines = f.readlines()
+
+calories_by_deer = [0]
+
+for i in lines:
+    if i == '\n':
+        calories_by_deer.append(0)
+    else:
+        calories_by_deer[-1] += int(i)
+
+print(max(calories_by_deer))
