cb0a9f058795e718195eed4dd5eba1344967895b
forden <forden@pop-os.localdomain>
2022-12-04 11:20:49 +0300

    day 4 part 1 completed

--- day4/solve.py
+++ day4/solve.py
@@ -1,3 +1,21 @@
 with open('input.txt') as f:
     lines = f.readlines()
 lines = [i.strip() for i in lines]
+
+
+def part_1():
+    result = 0
+    for pair in lines:
+        first, second = pair.split(',')
+        first_range = tuple(map(int, first.split('-')))
+        second_range = tuple(map(int, second.split('-')))
+        if first_range[0] >= second_range[0] and first_range[1] <= second_range[1]:
+            result += 1
+        elif first_range[0] <= second_range[0] and first_range[1] >= second_range[1]:
+            result += 1
+        else:
+            pass
+    return result
+
+
+print(part_1())
