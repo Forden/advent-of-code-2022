5bfa8c1c24d3499cbdc614e569742aaca915838b
forden <forden@pop-os.localdomain>
2022-12-04 16:51:52 +0300

    improved template

--- dayN/solve.py
+++ dayN/solve.py
@@ -1,3 +1,17 @@
 with open('input.txt') as f:
     lines = f.readlines()
 lines = [i.strip() for i in lines]
+
+
+def part_1():
+    result = 0
+    return result
+
+
+def part_2():
+    result = 0
+    return result
+
+
+print(part_1())
+print(part_2())
