e354f1b3d75929028d7c53598ad8a3882899717a
forden <forden@pop-os.localdomain>
2022-12-04 11:26:10 +0300

    day 4 part 2 completed

--- day4/solve.py
+++ day4/solve.py
@@ -18,4 +18,16 @@ def part_1():
     return result
 
 
+def part_2():
+    result = 0
+    for pair in lines:
+        first, second = pair.split(',')
+        first_range = tuple(map(int, first.split('-')))
+        second_range = tuple(map(int, second.split('-')))
+        if set([i for i in range(first_range[0], first_range[1]+1, 1)]).intersection([i for i in range(second_range[0], second_range[1]+1, 1)]):
+            result += 1
+    return result
+
+
 print(part_1())
+print(part_2())
