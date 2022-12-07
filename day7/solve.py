67d7fb98690ffacc830b83e724e2505c6255e87b
forden <forden@pop-os.localdomain>
2022-12-07 09:01:00 +0300

    day 7 part 2 completed

--- day7/solve.py
+++ day7/solve.py
@@ -51,10 +51,66 @@ def part_1():
                     disk[cwd] = {}
         elif i.startswith('$ ls'):
             output = []
-            output_ends = len(lines)
             for x_ind, x in enumerate(lines[line_ind + 1:]):
                 if x.startswith('$'):
-                    output_ends = x_ind
+                    break
+                else:
+                    output.append(x)
+            for x in output:
+                f_type, f_name = x.split()
+                if f_type == 'dir':
+                    if cwd + f_name + '/' not in disk:
+                        disk[cwd + f_name + '/'] = {}
+                else:
+                    if f_name not in disk[cwd]:
+                        disk[cwd][f_name] = int(f_type)
+    dir_sizes = {}
+    for k, v in disk.items():
+        dir_size = 0
+        if v:
+            for in_kv, in_v in v.items():
+                if isinstance(in_v, int):
+                    dir_size += in_v
+        dir_sizes[k] = dir_size
+    for k in dir_sizes.keys():
+        for in_k, in_v in dir_sizes.items():
+
+            if in_k == k:
+                continue
+            if in_k.startswith(k):
+                dir_sizes[k] += in_v
+    for k, v in dir_sizes.items():
+        if v <= 100000:
+            result += v
+    return result
+
+
+def part_2():
+    result = 0
+    total_disk_size = 70000000
+    minimum_needed = 30000000
+
+    disk = {
+        '/': {}
+    }
+    cwd = '~'
+    for line_ind, i in enumerate(lines):
+
+        if i.startswith('$ cd'):
+            _, cmd, args = i.split()
+            if args == '/':
+                cwd = '/'
+            elif args == '..':
+                cwd = '/'.join(cwd.split('/')[:-2])
+                cwd += '/'
+            else:
+                cwd += f'{args}/'
+                if cwd not in disk:
+                    disk[cwd] = {}
+        elif i.startswith('$ ls'):
+            output = []
+            for x_ind, x in enumerate(lines[line_ind + 1:]):
+                if x.startswith('$'):
                     break
                 else:
                     output.append(x)
@@ -74,33 +130,27 @@ def part_1():
             for in_kv, in_v in v.items():
                 if isinstance(in_v, int):
                     dir_size += in_v
-            if dir_size <= 100000:
-                print(f'{k=}, {dir_size=}')
         dir_sizes[k] = dir_size
-                # result += dir_size
-
-        # print(k, v)
     print(dir_sizes)
     for k in dir_sizes.keys():
         for in_k, in_v in dir_sizes.items():
-
             if in_k == k:
                 continue
             if in_k.startswith(k):
-                print(k, in_k)
                 dir_sizes[k] += in_v
     print(dir_sizes)
+    current_taken_space = dir_sizes['/']
+    unused_space = total_disk_size - current_taken_space
+    space_to_clean = minimum_needed - unused_space
+    print(space_to_clean)
+    print(current_taken_space)
+    dir_to_clean = '/'
     for k, v in dir_sizes.items():
-        if v <= 100000:
-            result += v
-    # print(sum(map(lambda x: x.values(), disk.values())))
-    return result
-
-
-def part_2():
-    result = 0
-
-    return result
+        if space_to_clean <= v < dir_sizes[dir_to_clean]:
+            dir_to_clean = k
+        # if v <= 100000:
+        #     result += v
+    return dir_sizes[dir_to_clean]
 
 
 print(part_1())
