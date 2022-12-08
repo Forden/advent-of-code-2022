3710c444ff3bd79e0450ada0b05e7a142100182f
forden <forden@pop-os.localdomain>
2022-12-08 08:37:38 +0300

    day 8 part 2 completed

--- day8/solve.py
+++ day8/solve.py
@@ -16,32 +16,66 @@ def part_1():
                 tree[1] = True
             else:
                 column = [i[tree_in_row_ind] for i in data]
-                print(
-                    f'tree={tree} up={column[:tree_row_ind]}, down={column[tree_row_ind + 1:]}, left={tree_row[:tree_in_row_ind]}, right={tree_row[tree_in_row_ind + 1:]}'
-                )
                 if all([i[0] < tree[0] for i in column[:tree_row_ind]]):
                     result += 1
                     tree[1] = True
-                    print('x', tree)
                 elif all([i[0] < tree[0] for i in column[tree_row_ind + 1:]]):
                     result += 1
                     tree[1] = True
-                    print('y', tree)
                 elif all([i[0] < tree[0] for i in tree_row[:tree_in_row_ind]]):
                     result += 1
                     tree[1] = True
-                    print('z', tree)
                 elif all([i[0] < tree[0] for i in tree_row[tree_in_row_ind + 1:]]):
                     result += 1
                     tree[1] = True
-                    print('a', tree)
-    # print()
-    # print(*data, sep='\n')
     return result
 
 
 def part_2():
     result = 0
+    data = [[[int(i), False, 0] for i in row] for row in lines]
+    for tree_row_ind, tree_row in enumerate(data):
+        for tree_in_row_ind, tree in enumerate(tree_row):
+            score = [0, 0, 0, 0]
+            column = [i[tree_in_row_ind] for i in data]
+            up = list(reversed(column[:tree_row_ind]))
+            down = column[tree_row_ind + 1:]
+            left = list(reversed(tree_row[:tree_in_row_ind]))
+            right = tree_row[tree_in_row_ind + 1:]
+            for i in up:
+                if i[0] >= tree[0]:
+                    score[0] += 1
+                    break
+                else:
+                    score[0] += 1
+            for i in down:
+                if i[0] >= tree[0]:
+                    score[2] += 1
+                    break
+                else:
+                    score[2] += 1
+            for i in left:
+                if i[0] >= tree[0]:
+                    score[1] += 1
+                    break
+                else:
+                    score[1] += 1
+            for i in right:
+                if i[0] >= tree[0]:
+                    score[3] += 1
+                    break
+                else:
+                    score[3] += 1
+            tree[2] = score[0] * score[1] * score[2] * score[3]
+            print(
+                f'tree={tree} up={up}, down={down}, left={left}, right={right} {score=}'
+            )
+    max_score = 4
+    for row in data:
+        for tree in row:
+            if tree[2] > max_score:
+                max_score = tree[2]
+    result = max_score
     return result
 
 
