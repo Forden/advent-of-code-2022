e777993c6526bbdfc4900f2aea27176ef616a975
forden <forden@pop-os.localdomain>
2022-12-10 08:23:08 +0300

    day 10 part 1 completed

--- day10/solve.py
+++ day10/solve.py
@@ -4,8 +4,29 @@ lines = [i.strip() for i in lines]
 
 
 def part_1():
-    result = 0
-    return result
+    results = []
+    optimized = []
+    for i in lines:
+        if i == 'noop':
+            optimized.append(i)
+        else:
+            optimized.append('noop')
+            optimized.append(' '.join(['add_to_x', *i.split()[1:]]))
+    cycle = 1
+    x_register = 1
+    for line in optimized:
+        # print(cycle, line, x_register)
+        if cycle in [20, 60, 100, 140, 180, 220]:
+            print(cycle, x_register)
+            results.append(cycle * x_register)
+        if line == 'noop':
+            cycle += 1
+        elif line.startswith('add_to_x'):
+            cmd, arg = line.split()
+            cycle += 1
+            x_register += int(arg)
+
+    return sum(results)
 
 
 def part_2():
