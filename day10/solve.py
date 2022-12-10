e82e09a8479d5e1d41ea0f13c89611f027f3f9ce
forden <forden@pop-os.localdomain>
2022-12-10 08:58:36 +0300

    day 10 part 2 completed

--- day10/solve.py
+++ day10/solve.py
@@ -15,7 +15,6 @@ def part_1():
     cycle = 1
     x_register = 1
     for line in optimized:
-        # print(cycle, line, x_register)
         if cycle in [20, 60, 100, 140, 180, 220]:
             print(cycle, x_register)
             results.append(cycle * x_register)
@@ -25,13 +24,40 @@ def part_1():
             cmd, arg = line.split()
             cycle += 1
             x_register += int(arg)
-
     return sum(results)
 
 
 def part_2():
-    result = 0
-    return result
+    results = []
+    optimized = []
+    for i in lines:
+        if i == 'noop':
+            optimized.append(i)
+        else:
+            optimized.append('noop')
+            optimized.append(' '.join(['add_to_x', *i.split()[1:]]))
+    grid = [[' ' for _ in range(40)] for _ in range(6)]
+    cycle = 1
+    x_register = 1
+    for line in optimized:
+        grid_row = (cycle - 1) // 40
+        x = (cycle - 1) % 40
+        if grid[grid_row][x] == ' ':
+            if x_register - 1 <= x <= x_register + 1:
+                grid[grid_row][x] = '#'
+
+        if cycle <= 21:
+            print(cycle, x_register, cycle - 1 % 40, grid[grid_row])
+        if cycle in [20, 60, 100, 140, 180, 220]:
+            results.append(cycle * x_register)
+        if line == 'noop':
+            cycle += 1
+        elif line.startswith('add_to_x'):
+            cmd, arg = line.split()
+            cycle += 1
+            x_register += int(arg)
+    print(*[''.join(map(str, i)).replace(' ', '.') for i in grid], sep='\n')
+    return sum(results)
 
 
 print(part_1())
