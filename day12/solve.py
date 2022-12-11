3b5b148a41244ad9c3f0026f4d31daaf2d7f8cf4
forden <forden@pop-os.localdomain>
2022-12-11 22:11:47 +0300

    day 12 prepared

---
+++ day12/solve.py
@@ -0,0 +1,40 @@
+import typing
+
+
+def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
+    result = 0
+    return result
+
+
+def part_2(input_lines: typing.List[str]) -> typing.Union[int, str]:
+    result = 0
+    return result
+
+
+# noinspection PyBroadException
+def read_file_lines(file_to_read: str, strip: bool = True) -> typing.List[str]:
+    try:
+        with open(file_to_read) as f:
+            lines = f.readlines()
+    except:
+        return []
+    if strip:
+        lines = [i.strip() for i in lines]
+    return lines.copy()
+
+
+if __name__ == '__main__':
+    files_to_run = [
+        'sample.txt',
+        'input.txt'
+    ]
+    for filename in files_to_run:
+        file_content = read_file_lines(filename, strip=True)
+        if len(file_content) == 0:
+            print(f'nothing in file {filename}, skipping')
+            continue
+        print(f'part 1 for {filename}')
+        print(part_1(file_content.copy()))
+        print(f'part 2 for {filename}')
+        print(part_2(file_content.copy()))
+        print()
