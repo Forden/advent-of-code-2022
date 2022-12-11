9a47d491aafe955237b2c8462650413d62440f8f
forden <forden@pop-os.localdomain>
2022-12-11 06:59:06 +0300

    day 11 prepared

--- day11/solve.py
+++ day11/solve.py
@@ -1,17 +1,35 @@
-with open('input.txt') as f:
-    lines = f.readlines()
-lines = [i.strip() for i in lines]
+import typing
 
 
-def part_1():
+def part_1(input_lines: typing.List[str]):
     result = 0
     return result
 
 
-def part_2():
+def part_2(input_lines: typing.List[str]):
     result = 0
     return result
 
 
-print(part_1())
-print(part_2())
+def read_file_lines(filename: str, strip: bool = True) -> typing.List[str]:
+    with open(filename) as f:
+        lines = f.readlines()
+    if strip:
+        lines = [i.strip() for i in lines]
+    return lines.copy()
+
+
+if __name__ == '__main__':
+    files_to_run = [
+        'sample.txt',
+        'input.txt'
+    ]
+    for filename in files_to_run:
+        file_content = read_file_lines(filename, strip=True)
+        if not file_content:
+            print(f'nothing in file {filename}, skipping')
+        print(f'part 1 for {filename}')
+        print(part_1(file_content.copy()))
+        print(f'part 2 for {filename}')
+        print(part_2(file_content.copy()))
+        print()
