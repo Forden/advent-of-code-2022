c3b8772ac1c19872bbf3faed9d4bbcc9aa995bf4
forden <forden@pop-os.localdomain>
2022-12-12 09:46:49 +0300

    day 12 part 2 completed

--- day12/solve.py
+++ day12/solve.py
@@ -93,6 +93,7 @@ def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
         print()
         for i in range(len(path) - 1, -1, -1):
             print(grid[graph_short[path[i]][1]][graph_short[path[i]][0]], end=' ')
+        return dist[dest]
 
     current = (0, 0)
     target = (0, 0)
@@ -126,14 +127,147 @@ def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
                 add_connection(ax, str((a, b)), i)
 
     dot_amount = len(grid) * len(grid[0])
-    get_shortest_path_length(grid, connections, coords_to_short[str(current)], coords_to_short[str(target)], dot_amount)
-    result = 0
+    result = get_shortest_path_length(
+        grid, connections, coords_to_short[str(current)], coords_to_short[str(target)], dot_amount
+    )
+
     return result
 
 
 def part_2(input_lines: typing.List[str]) -> typing.Union[int, str]:
+    def get_all_connections(grid, x, y):
+        x_to_check = [x]
+        y_to_check = [y]
+        if x == 0:
+            x_to_check.append(x + 1)
+        elif x == len(grid[0]) - 1:
+            x_to_check.append(x - 1)
+        else:
+            x_to_check.append(x + 1)
+            x_to_check.append(x - 1)
+        if y == 0:
+            y_to_check.append(y + 1)
+        elif y == len(grid) - 1:
+            y_to_check.append(y - 1)
+        else:
+            y_to_check.append(y + 1)
+            y_to_check.append(y - 1)
+        current_value = grid[y][x]
+        res = []
+
+        for checking_x in x_to_check:
+            for checking_y in y_to_check:
+                if checking_y == y and checking_x == x:
+                    continue
+                if checking_x != x and checking_y != y:
+                    continue
+                # print(x, y, checking_x, checking_y)
+
+                if grid[checking_y][checking_x] <= current_value + 1:
+                    res.append((checking_x, checking_y))
+        return res
+
+    def add_connection(arr, source, dest):
+        if source not in arr:
+            arr[source] = []
+        if dest not in arr[source]:
+            arr[source].append(dest)
+        # if dest not in arr:
+        #     arr[dest] = []
+        # if source not in arr[dest]:
+        #     arr[dest].append(source)
+
+    def bfs(grd, arr, source, dest, dots_amount, pred, dist):
+        queue = []
+        visited = [False for _ in range(dots_amount)]
+        for i in range(dots_amount):
+            dist[i] = 1000000
+            pred[i] = -1
+        visited[source] = True
+        dist[source] = 0
+        queue.append(source)
+        while len(queue) != 0:
+            u = queue.pop(0)
+            if u not in arr:
+                continue
+            # print(u, arr[u])
+            for i in range(len(arr[u])):
+                if visited[arr[u][i]] is False:
+                    visited[arr[u][i]] = True
+                    dist[arr[u][i]] = dist[u] + 1
+                    pred[arr[u][i]] = u
+                    queue.append(arr[u][i])
+                    if arr[u][i] == dest:
+                        return True
+        return False
+
+    def get_shortest_path_length(grd, arr, source, dest, dots_amount):
+        pred = [0 for _ in range(dots_amount)]
+        dist = [0 for _ in range(dots_amount)]
+        if bfs(grd, arr, source, dest, dots_amount, pred, dist) is False:
+            # print('path not found')
+            pass
+        path = []
+        crawl = dest
+        path.append(crawl)
+
+        while pred[crawl] != -1:
+            path.append(pred[crawl])
+            crawl = pred[crawl]
+        # print('shortest path:', dist[dest])
+        # print("\nPath is : : ")
+
+        # for i in range(len(path) - 1, -1, -1):
+        #     print(graph_short[path[i]], end=' ')
+        # print()
+        # for i in range(len(path) - 1, -1, -1):
+        #     print(grid[graph_short[path[i]][1]][graph_short[path[i]][0]], end=' ')
+        return dist[dest]
+
+    current = (0, 0)
+    target = (0, 0)
+    for y, _ in enumerate(input_lines):
+        for x, _ in enumerate(input_lines[y]):
+            if input_lines[y][x] == 'S':
+                current = (x, y)
+            elif input_lines[y][x] == 'E':
+                target = (x, y)
+    input_lines[current[1]] = input_lines[current[1]][:current[0]] + 'a' + input_lines[current[1]][current[0] + 1:]
+    input_lines[target[1]] = input_lines[target[1]][:target[0]] + 'z' + input_lines[target[1]][target[0] + 1:]
+    grid = [[get_dot_height(i) for i in row] for row in input_lines]
+    graph_short = []
+    for y, _ in enumerate(grid):
+        for x, _ in enumerate(grid[y]):
+            graph_short.append((x, y))
+    coords_to_short = {}
+    for ind, i in enumerate(graph_short):
+        coords_to_short[str(i)] = ind
+    connections = {}
+    counter = 0
+    for b, _ in enumerate(input_lines):
+        for a, _ in enumerate(input_lines[b]):
+            for i in get_all_connections(grid, a, b):
+                add_connection(connections, counter, coords_to_short[str(i)])
+            counter += 1
+    ax = {}
+    for b, _ in enumerate(input_lines):
+        for a, _ in enumerate(input_lines[b]):
+            for i in get_all_connections(grid, a, b):
+                add_connection(ax, str((a, b)), i)
+
+    dot_amount = len(grid) * len(grid[0])
     result = 0
-    return result
+    results = {}
+    for y, _ in enumerate(grid):
+        for x, _ in enumerate(grid[y]):
+            if grid[y][x] == 1:
+                results[(x, y)] = -1
+    for i in results:
+        results[i] = get_shortest_path_length(
+            grid, connections, coords_to_short[str(i)], coords_to_short[str(target)], dot_amount
+        )
+    print(results.values())
+    return min(results.values())
 
 
 # noinspection PyBroadException
