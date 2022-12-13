1b7b7477c41af0d4a1bb0d4159ab825aa9f51fb6
forden <forden@pop-os.localdomain>
2022-12-13 09:26:31 +0300

    day 13 part 1&2 completed

--- day13/solve.py
+++ day13/solve.py
@@ -1,13 +1,66 @@
+import functools
+import json
 import typing
+
+
+def chunks(list_to_split, chunk_size):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(list_to_split), chunk_size):
+        yield list_to_split[i:i + chunk_size]
+
+
+def compare(first, second):
+    if isinstance(first, int) and isinstance(second, int):
+        if first < second:
+            return 1
+        elif first == second:
+            return 0
+        else:
+            return -1
+    elif isinstance(first, list) and isinstance(second, list):
+        for i in range(min(len(first), len(second))):
+            compare_res = compare(first[i], second[i])
+            if compare_res == 1:
+                return 1
+            elif compare_res == -1:
+                return -1
+        if len(first) < len(second):
+            return 1
+        elif len(first) > len(second):
+            return -1
+        else:
+            return 0
+    elif isinstance(first, int) and isinstance(second, list):
+        return compare([first], second)
+    else:
+        return compare(first, [second])
 
 
 def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
     result = 0
+    for pair_ind, pair in enumerate(chunks(input_lines, 3)):
+        first, second, _ = pair
+        first = json.loads(first)
+        second = json.loads(second)
+        if compare(first, second) == 1:
+            result += pair_ind + 1
     return result
 
 
 def part_2(input_lines: typing.List[str]) -> typing.Union[int, str]:
-    result = 0
+    data = [[2], [6]]
+    for pair_ind, pair in enumerate(chunks(input_lines, 3)):
+        first, second, _ = pair
+        first = json.loads(first)
+        second = json.loads(second)
+        data.extend([first, second])
+    data = sorted(data, key=functools.cmp_to_key(lambda a, b: compare(a, b)), reverse=True)
+    result = 1
+    for ind, i in enumerate(data):
+        if i == [2]:
+            result *= ind + 1
+        elif i == [6]:
+            result *= ind + 1
     return result
 
 
