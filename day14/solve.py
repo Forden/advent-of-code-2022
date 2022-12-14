9f62d635be0af1e8e876c1931ecbd19f69d897c4
forden <forden@pop-os.localdomain>
2022-12-14 09:41:29 +0300

    day 14 part 1 completed

--- day14/solve.py
+++ day14/solve.py
@@ -1,9 +1,83 @@
 import typing
+
 import utils
 
 
 def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
     result = 0
+    min_x, max_x, max_y = 100000, 0, 0
+    for path in input_lines:
+        for pair in utils.sliding_window(
+                list(map(lambda x: (int(x[0]), int(x[1])), map(lambda x: tuple(x.split(',')), path.split(' -> ')))),
+                2,
+                1
+        ):
+            if len(pair) == 1:
+                continue
+            for x, y in pair:
+                if x < min_x:
+                    min_x = x
+                if x > max_x:
+                    max_x = x
+                if y > max_y:
+                    max_y = y
+    print(min_x, max_x, max_y)
+    min_x -= 1
+    offset_x = min_x
+    grid = [['.' for _ in range(max_x - offset_x)] for _ in range(max_y + 1)]
+    for path in input_lines:
+        for pair in utils.sliding_window(
+                list(map(lambda x: (int(x[0]), int(x[1])), map(lambda x: tuple(x.split(',')), path.split(' -> ')))),
+                2,
+                1
+        ):
+            if len(pair) == 1:
+                continue
+            if pair[0][0] == pair[1][0]:
+                if pair[0][1] <= pair[1][1]:
+                    for i in range(pair[0][1], pair[1][1]):
+                        grid[i][pair[0][0] - offset_x - 1] = '#'
+                        pass
+                elif pair[0][1] >= pair[1][1]:
+                    for i in range(pair[1][1], pair[0][1]):
+                        grid[i][pair[0][0] - offset_x - 1] = '#'
+            elif pair[0][0] >= pair[1][0]:
+                for i in range(pair[1][0], pair[0][0] + 1):
+                    grid[pair[0][1]][i - offset_x - 1] = '#'
+            elif pair[0][0] < pair[1][0]:
+                for i in range(pair[0][0], pair[1][0] + 1):
+                    grid[pair[0][1]][i - offset_x - 1] = '#'
+            else:
+                print(f'weird {pair}')
+    sand_pos = (500 - offset_x - 1, 0)
+    grid[0][500 - offset_x - 1] = '+'
+    print(*[''.join(map(str, i)) for i in grid], sep='\n')
+    while '+' in ''.join(''.join(i) for i in grid):
+        for y, _ in enumerate(grid):
+            for x, _ in enumerate(grid[y]):
+                if grid[y][x] == '+':
+                    sand_pos = (x + offset_x + 1, y)
+        if sand_pos[1] >= max_y:
+            break
+        # print(sand_pos)
+        if grid[sand_pos[1] + 1][sand_pos[0] - offset_x - 1] == '.':
+            grid[sand_pos[1] + 1][sand_pos[0] - offset_x - 1] = '+'
+            grid[sand_pos[1]][sand_pos[0] - offset_x - 1] = '.'
+        elif grid[sand_pos[1] + 1][sand_pos[0] - offset_x - 1] in ['o', '#']:
+            if grid[sand_pos[1] + 1][sand_pos[0] - offset_x - 2] == '.':
+                grid[sand_pos[1] + 1][sand_pos[0] - offset_x - 2] = '+'
+                grid[sand_pos[1]][sand_pos[0] - offset_x - 1] = '.'
+            elif grid[sand_pos[1] + 1][sand_pos[0] - offset_x] == '.':
+                grid[sand_pos[1] + 1][sand_pos[0] - offset_x] = '+'
+                grid[sand_pos[1]][sand_pos[0] - offset_x - 1] = '.'
+            else:
+                grid[sand_pos[1]][sand_pos[0] - offset_x - 1] = 'o'
+        if ('+' not in ''.join(''.join(i) for i in grid)) and (''.join(''.join(i) for i in grid).count('o') <= 10000):
+            grid[0][500 - offset_x - 1] = '+'
+        print()
+        print(*[''.join(map(str, i)) for i in grid], sep='\n')
+    # print(*[''.join(map(str, i)) for i in grid], sep='\n')
+    result = ''.join(''.join(i) for i in grid).count('o')
     return result
 
 
