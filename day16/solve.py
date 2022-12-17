940f3f23e9479c6890040b2e3bc37f7894e57520
forden <forden@pop-os.localdomain>
2022-12-17 17:21:38 +0300

    day 16 part 1 code cleanup

--- day16/solve.py
+++ day16/solve.py
@@ -18,42 +18,42 @@ def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
     for k in sorted(graph.keys()):
         valve_to_num[k] = len(valve_to_num.values()) + 1
         num_to_valve[len(valve_to_num.values())] = k
-    print(f'{valve_to_num=}')
-    print(f'{num_to_valve=}')
-    print(f'{graph=}')
 
     TOTAL_TIME = 30
 
-    states: list[tuple[int, set, int]] = [(valve_to_num['AA'], set(), 0)]
+    states: list[tuple[int, set, int]] = [
+        (
+            valve_to_num['AA'], set(
+                filter(
+                    lambda x: valves_rates[num_to_valve[x]] <= 0,
+                    [valve_to_num[i] for i in valve_to_num]
+                )
+            ),
+            0
+        )
+    ]  # open all zero valves
     best_orders = {}
 
     # cursed bfs
     for t in range(1, TOTAL_TIME + 1):
-        print(f'{t=}, {len(states)=} {states=}')
-
         new_states = []
         for current_location, opened, total_pressure_released in states:
-            print(f'{current_location=}, {opened=}, {total_pressure_released=}')
             key = (current_location, ','.join(map(str, opened)))
             if key in best_orders:
                 if total_pressure_released <= best_orders[key]:
-                    print('skipped because already checked and no better')
                     continue
                 else:
-                    print(f'found better order with {total_pressure_released=} (old pressure={best_orders[key]})')
+                    pass
 
             best_orders[key] = total_pressure_released
 
             flow_rate = valves_rates[num_to_valve[current_location]]
             children = graph[num_to_valve[current_location]]
             if current_location in opened:
-                print('skipping because already opened')
+                pass
             elif flow_rate <= 0:
-                print(f'skipping because {flow_rate=}')
+                pass
             else:
-                print(
-                    f'opening, pressure += {flow_rate * (TOTAL_TIME - t)} with new presssure={total_pressure_released + flow_rate * (TOTAL_TIME - t)}'
-                )
                 new_states.append(
                     (
                         current_location,
@@ -64,13 +64,31 @@ def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
             for dest in children:
                 new_states.append((valve_to_num[dest], opened, total_pressure_released))
         states = new_states
-    answer = max(pressure for _, _, pressure in states)
-    return answer
+    return max(best_orders.values())
 
 
 def part_2(input_lines: typing.List[str]) -> typing.Union[int, str]:
-    result = 0
-    return result
+    graph = {}
+    valves_rates = {}
+    valve_to_num = {}
+    num_to_valve = {}
+    for i in input_lines:
+        _, name, _, _, rate_raw = i.split('; ')[0].split()
+        rate = int(rate_raw.split('=')[1])
+        second_part = i.split('; ')[1].split()
+        second_part = second_part[4:]
+        valves_rates[name] = rate
+        graph[name] = list(map(lambda x: x.rstrip(','), second_part))
+    for k in sorted(graph.keys()):
+        valve_to_num[k] = len(valve_to_num.values()) + 1
+        num_to_valve[len(valve_to_num.values())] = k
+    print(f'{valve_to_num=}')
+    print(f'{num_to_valve=}')
+    print(f'{graph=}')
+
+    TOTAL_TIME = 26
+
+    return 0
 
 
 def read_file_lines(file_to_read: str, strip: bool = True) -> typing.List[str]:
@@ -86,7 +104,7 @@ def read_file_lines(file_to_read: str, strip: bool = True) -> typing.List[str]:
 
 if __name__ == '__main__':
     files_to_run = [
-        'sample.txt',
+        # 'sample.txt',
         'input.txt'
     ]
     for filename in files_to_run:
