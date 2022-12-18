8550d78a1883c2aabdafa01867c136a68ece77e9
forden <forden@pop-os.localdomain>
2022-12-18 08:54:40 +0300

    day 18 part 1 completed

--- day18/solve.py
+++ day18/solve.py
@@ -1,9 +1,44 @@
+import dataclasses
 import typing
-import utils
 
 
 def part_1(input_lines: typing.List[str]) -> typing.Union[int, str]:
+    @dataclasses.dataclass
+    class Cube:
+        x: int
+        y: int
+        z: int
+
+        def __hash__(self):
+            return hash((self.x, self.y, self.z))
+
     result = 0
+
+    def get_cube_exposed_sides_count(
+            cube: Cube, grid: typing.Set[Cube]
+    ) -> int:
+        res = 6
+        for i in grid:
+            if i.x == cube.x + 1 and i.y == cube.y and i.z == cube.z:
+                res -= 1
+            elif i.x == cube.x - 1 and i.y == cube.y and i.z == cube.z:
+                res -= 1
+            elif i.x == cube.x and i.y == cube.y + 1 and i.z == cube.z:
+                res -= 1
+            elif i.x == cube.x and i.y == cube.y - 1 and i.z == cube.z:
+                res -= 1
+            elif i.x == cube.x and i.y == cube.y and i.z == cube.z + 1:
+                res -= 1
+            elif i.x == cube.x and i.y == cube.y and i.z == cube.z - 1:
+                res -= 1
+        return res
+
+    cubes = set()
+    for cube in input_lines:
+        x, y, z, = map(int, cube.split(','))
+        cubes.add(Cube(x, y, z))
+    for i in cubes:
+        result += get_cube_exposed_sides_count(i, cubes)
     return result
 
 
